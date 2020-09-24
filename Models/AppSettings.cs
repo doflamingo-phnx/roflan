48da72b064b967929535f0666ff41936e7720af7
doflamingo-phnx <doflamingo-phnx@users.noreply.github.com>
2020-09-24 17:27:41 +0300

    1

---
+++ Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstBot.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; } = "https://telegrambotapp.azurewebsites.net:433/{0}";
+        public static string Name { get; set; } = "mingo_bot";
+        public static string Key { get; set; } = "1182414567:AAFTVg1bJcErTzf3BaY0zXSuOhZYfpf0Ns8";
+    }
+}
---
+++ Models/Bot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using FirstBot.Models.Commands;
+
+namespace FirstBot.Models
+{
+    public static class Bot
+    {
+        private static TelegramBotClient client;
+        private static List<Command> commandsList;
+
+
+        public static IReadOnlyList<Command> Commands { get => commandsList.AsReadOnly(); }
+
+
+        public static async Task<TelegramBotClient> Get()
+        {
+            if(client != null)
+            {
+                return client;
+            }
+
+            commandsList = new List<Command>();
+            commandsList.Add(new HelloCommand());
+            // new commands
+
+
+
+            client = new TelegramBotClient(AppSettings.Key);
+            var hook = string.Format(AppSettings.Url, "api/message/update");
+            await client.SetWebhookAsync(hook);
+
+            return client;
+        }
+    }
+}
